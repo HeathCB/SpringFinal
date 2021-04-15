e1f377b3e1f21466bd03d68739cd7331ee3b7cff
Heath C Brown <Heath.c.b@gmail.com>
2021-04-14 21:39:23 -0500

    Delete restaurantManagement directory

--- restaurantManagement/src/main/java/com/promineotech/restaurantManagement/src/main/java/com/promineotech/restaurantManagement/repository/RestaurantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.promineotech.restaurantManagement.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.promineotech.restaurantManagement.entity.Restaurant;
-
-public interface RestaurantRepository extends CrudRepository<Restaurant, Long>  {
-
-}
